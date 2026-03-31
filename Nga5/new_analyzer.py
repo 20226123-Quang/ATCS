26cfac8d148f7933c8597d5554e82b04b51cfd4d
quyenaoy <Quyen.PH225224@sis.hust.edu.vn>
2026-03-31 16:38:55 +0700

    Debug tính kpi done

--- Nga5/new_analyzer.py
+++ Nga5/new_analyzer.py
@@ -4,7 +4,7 @@ import os
 
 def analyze_all_data_kpis(folder_path="."):
     # 1. Tìm tất cả các file crowd_*_ngabavoi.csv
-    all_files = glob.glob(os.path.join(folder_path, "crowd_*_nga5.csv"))
+    all_files = glob.glob(os.path.join(folder_path, "debug_nga5.csv"))
     
     if not all_files:
         print(f"[-] Không tìm thấy file dữ liệu nào tại: {os.path.abspath(folder_path)}")
@@ -62,8 +62,8 @@ def analyze_all_data_kpis(folder_path="."):
     print("="*110 + "\n")
 
     # Xuất file báo cáo tổng hợp
-    summary_df.to_csv("crowd_nga5.csv", index=False)
-    print("[*] Đã lưu báo cáo đầy đủ tại: crowd_nga5.csv")
+    summary_df.to_csv("huhu_nga5.csv", index=False)
+    print("[*] Đã lưu báo cáo đầy đủ tại: huhu_nga5.csv")
 
 if __name__ == "__main__":
     analyze_all_data_kpis()
